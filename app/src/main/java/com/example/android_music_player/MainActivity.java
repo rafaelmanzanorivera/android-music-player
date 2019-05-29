3baabb63e7f9b8b3ec9e9e7630f90d270f31413b
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-29 23:44:56 +0200

    Fixed songs extra functionality

--- app/src/main/java/com/example/android_music_player/MainActivity.java
+++ app/src/main/java/com/example/android_music_player/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android_music_player;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.android_music_player.library.view_pager.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        ViewPager pager = findViewById(R.id.viewpager);
+        pager.setAdapter(adapter);
     }
 }
