50dea2631a58057bd1fd819786c25ba86cc42500
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-30 00:29:16 +0200

    Added ViewPager tabBar

--- app/src/main/java/com/example/android_music_player/MainActivity.java
+++ app/src/main/java/com/example/android_music_player/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android_music_player;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,13 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        //View pager conf
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),this);
 
         ViewPager pager = findViewById(R.id.viewpager);
         pager.setAdapter(adapter);
 
+
         //Toolbar conf
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(pager);
+
     }
 }
--- app/src/main/java/com/example/android_music_player/library/view_pager/ViewPagerAdapter.java
+++ app/src/main/java/com/example/android_music_player/library/view_pager/ViewPagerAdapter.java
@@ -1,17 +1,23 @@
 package com.example.android_music_player.library.view_pager;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.example.android_music_player.R;
 import com.example.android_music_player.library.Albums_Fr;
 import com.example.android_music_player.library.Songs_Fr;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
-    public ViewPagerAdapter(FragmentManager fm) {
+    private Context m_context;
+
+    public ViewPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
+        m_context = context;
     }
+
 
     @Override
     public Fragment getItem(int i) {
@@ -31,4 +37,17 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     public int getCount() {
         return 2;
     }
+
+    public CharSequence getPageTitle(int position)
+    {
+        switch(position)
+        {
+            case 0:
+                return m_context.getResources().getText(R.string.library_tab);
+            case 1:
+                return m_context.getResources().getText(R.string.songs_tab);
+        }
+        return null;
+    }
+
 }
