e02596389c842b8c2ea795fe47e93222f185f96f
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-30 15:49:54 +0200

    Fixed song layout

--- app/src/main/java/com/example/android_music_player/library/SongsRecyclerAdapter.java
+++ app/src/main/java/com/example/android_music_player/library/SongsRecyclerAdapter.java
@@ -4,7 +4,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.android_music_player.R;
@@ -18,11 +20,11 @@ public class SongsRecyclerAdapter extends RecyclerView.Adapter<SongsRecyclerAdap
 
     public static class MyViewHolder extends RecyclerView.ViewHolder
     {
-        public LinearLayout linearLayout;
-        public MyViewHolder(LinearLayout v)
+        public RelativeLayout gridLayout;
+        public MyViewHolder(RelativeLayout v)
         {
             super(v);
-            linearLayout = v;
+            gridLayout = v;
         }
     }
 
@@ -33,15 +35,15 @@ public class SongsRecyclerAdapter extends RecyclerView.Adapter<SongsRecyclerAdap
     @NonNull
     @Override
     public SongsRecyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        LinearLayout v = (LinearLayout) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.song_list_item,viewGroup,false);
+        RelativeLayout v = (RelativeLayout) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.song_list_item,viewGroup,false);
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i){
-        ((TextView) myViewHolder.linearLayout.findViewById(R.id.item_song_name)).setText(dataset.get(i).getaName());
-        ((TextView) myViewHolder.linearLayout.findViewById(R.id.item_song_duration)).setText(dataset.get(i).getaAlbum());
+        ((TextView) myViewHolder.gridLayout.findViewById(R.id.item_song_name)).setText(dataset.get(i).getaName());
+        ((TextView) myViewHolder.gridLayout.findViewById(R.id.item_song_duration)).setText(dataset.get(i).getaAlbum());
     }
 
     @Override
