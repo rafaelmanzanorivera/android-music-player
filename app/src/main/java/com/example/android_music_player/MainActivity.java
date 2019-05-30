398f661adfb8f11d1717db4fee87f340ea132b41
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-30 03:16:33 +0200

    Audio model introduced

--- app/src/main/java/com/example/android_music_player/MainActivity.java
+++ app/src/main/java/com/example/android_music_player/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.android_music_player;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -9,10 +13,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.android_music_player.library.view_pager.ViewPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -89,5 +97,47 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             drawerLayout.closeDrawer(GravityCompat.START);
         else
             super.onBackPressed();
+    }
+
+    
+    // Method to read all the audio/MP3 files.
+    public List<AudioModel> getAllAudioFromDevice(final Context context) {
+        final List<AudioModel> tempAudioList = new ArrayList<>();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Audio.AudioColumns.DATA,MediaStore.Audio.AudioColumns.TITLE ,MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST,};
+        Cursor c = context.getContentResolver().query(uri,
+                projection,
+                null,
+                null,
+                null);
+
+        if (c != null) {
+            while (c.moveToNext()) {
+                // Create a model object.
+                AudioModel audioModel = new AudioModel();
+
+                String path = c.getString(0);   // Retrieve path.
+                String name = c.getString(1);   // Retrieve name.
+                String album = c.getString(2);  // Retrieve album name.
+                String artist = c.getString(3); // Retrieve artist name.
+
+                // Set data to the model object.
+                audioModel.setaName(name);
+                audioModel.setaAlbum(album);
+                audioModel.setaArtist(artist);
+                audioModel.setaPath(path);
+
+                Log.e("Name :" + name, " Album :" + album);
+                Log.e("Path :" + path, " Artist :" + artist);
+
+                // Add the model object to the list .
+                tempAudioList.add(audioModel);
+            }
+            c.close();
+        }
+
+        // Return the list.
+        return tempAudioList;
     }
 }
