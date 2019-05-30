8a31232ee7f3a444466651f80b18c6655d24d9d6
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-31 00:16:46 +0200

    DirectoryAdapter click listener

--- app/src/main/java/com/example/android_music_player/library/directories/DirectoryRecyclerAdapter.java
+++ app/src/main/java/com/example/android_music_player/library/directories/DirectoryRecyclerAdapter.java
@@ -1,13 +1,18 @@
 package com.example.android_music_player.library.directories;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.android_music_player.MainActivity;
 import com.example.android_music_player.R;
 import com.example.android_music_player.data.DirectoryModel;
 
@@ -19,15 +24,43 @@ public class DirectoryRecyclerAdapter extends RecyclerView.Adapter<DirectoryRecy
     private List<DirectoryModel> dataset;
     private ItemClickListener mClickListener;
 
-    public DirectoryRecyclerAdapter(List<DirectoryModel> dataset){ this.dataset = dataset; }
+    private Context context;
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder
+    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+
+    public DirectoryRecyclerAdapter(List<DirectoryModel> dataset, Context context){ this.dataset = dataset; this.context = context; }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
     {
         public RelativeLayout relativeLayout;
-        public MyViewHolder(RelativeLayout v)
+        public ImageView dirImage;
+        private Context context;
+
+        public MyViewHolder(RelativeLayout v, Context c)
         {
             super(v);
+            context = c;
             relativeLayout = v;
+
+            dirImage = (ImageView) v.findViewById(R.id.dir_image);
+
+            dirImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context, v+"js" , Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -36,8 +69,10 @@ public class DirectoryRecyclerAdapter extends RecyclerView.Adapter<DirectoryRecy
     public DirectoryRecyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
     {
         RelativeLayout v = (RelativeLayout) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.directory_grid_item,viewGroup,false);
-        MyViewHolder vh = new MyViewHolder(v);
+        MyViewHolder vh = new MyViewHolder(v,context);
         return vh;
+
+
     }
 
 
@@ -53,7 +88,6 @@ public class DirectoryRecyclerAdapter extends RecyclerView.Adapter<DirectoryRecy
     public int getItemCount() {
         return dataset.size();
     }
-
 
     // allows clicks events to be caught
     void setClickListener(ItemClickListener itemClickListener) {
