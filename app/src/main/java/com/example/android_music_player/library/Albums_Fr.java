bb404ddfc9d4ef9ab0ce473893e5d02f98d6f9cf
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-30 20:14:57 +0200

    Implemented grid logic in fragment

--- app/src/main/java/com/example/android_music_player/library/Albums_Fr.java
+++ app/src/main/java/com/example/android_music_player/library/Albums_Fr.java
@@ -1,12 +1,18 @@
 package com.example.android_music_player.library;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android_music_player.MainActivity;
 import com.example.android_music_player.R;
 
 /**
@@ -27,4 +33,15 @@ public class Albums_Fr extends Fragment {
         return inflater.inflate(R.layout.fragment_albums, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        RecyclerView recyclerView = (RecyclerView) getView().findViewById(R.id.directory_grid);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getView().getContext(),3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DirectoryRecyclerAdapter adapter = new DirectoryRecyclerAdapter(MainActivity.mediaData.getDirectoriesList());
+        recyclerView.setAdapter(adapter);
+    }
 }
