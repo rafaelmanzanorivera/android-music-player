107b35dc61ca4a6051d502860ed05ed32b3ec78c
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-31 22:55:49 +0200

    db connection to list + add playlist

---
+++ app/src/main/java/com/example/android_music_player/playlists/Playlists.java
@@ -0,0 +1,69 @@
+package com.example.android_music_player.playlists;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.android_music_player.MainActivity;
+import com.example.android_music_player.R;
+public class Playlists extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlists);
+
+        android.support.v7.widget.Toolbar tool = findViewById(R.id.toolbar);
+        setSupportActionBar(tool);
+        tool.setTitleTextColor(getResources().getColor(R.color.colorAccent));
+        getSupportActionBar().setTitle("Playlists");
+
+        //Nav bar
+        DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,tool,0,0);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigationdrawer_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.navigation_playlists);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        switch (id)
+        {
+            case R.id.navigation_library:
+                finishActivity(0);
+                break;
+
+            case R.id.navigation_playlists:
+                break;
+
+            case R.id.navigation_options:
+                break;
+
+            case R.id.navigation_contact:
+                break;
+
+            case R.id.navigation_help:
+                break;
+
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerlayout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+}
