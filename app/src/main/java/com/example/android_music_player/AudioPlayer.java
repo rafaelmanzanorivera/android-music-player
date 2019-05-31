b0f87b09780ecc3b16e6bd12937b1c87cb3d7036
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-31 04:11:10 +0200

    Created audio player class

---
+++ app/src/main/java/com/example/android_music_player/AudioPlayer.java
@@ -0,0 +1,87 @@
+package com.example.android_music_player;
+
+import android.media.MediaPlayer;
+import android.media.PlaybackParams;
+import android.widget.RelativeLayout;
+
+import com.example.android_music_player.data.AudioModel;
+
+public class AudioPlayer
+{
+    public MediaPlayer mediaPlayer;
+    public AudioModel playingSong;
+    public RelativeLayout playingSongLayout;
+
+    public AudioPlayer()
+    {
+        mediaPlayer = new MediaPlayer();
+    }
+
+    public void play(AudioModel song)
+    {
+
+        if(mediaPlayer.isPlaying())
+        {
+            //Different song
+            if(!song.getaName().equals(playingSong.getaName()))
+            {
+                //Stop other and start new
+                mediaPlayer.reset();
+
+                try {
+                    mediaPlayer.setDataSource(song.getaPath());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    playingSong = song;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else
+                mediaPlayer.start();
+        }
+        else if (playingSong != null)
+        {
+            if(!song.getaName().equals(playingSong.getaName()))
+            {
+                //Stop other and start new
+                mediaPlayer.reset();
+
+                try {
+                    mediaPlayer.setDataSource(song.getaPath());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    playingSong = song;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+                mediaPlayer.start();
+        }
+        else
+        {
+            //Stop other and start new
+            mediaPlayer.reset();
+
+            try {
+                mediaPlayer.setDataSource(song.getaPath());
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                playingSong = song;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void pause()
+    {
+        if(mediaPlayer.isPlaying())
+            mediaPlayer.pause();
+    }
+
+
+}
