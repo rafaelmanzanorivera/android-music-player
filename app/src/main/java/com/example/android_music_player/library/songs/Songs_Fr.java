01d27433070c28f055471c21df206dcbd325fd4d
Rafael Manzano <rafa.manzano@icloud.com>
2019-05-31 04:13:26 +0200

    Player icon listeners + logic

--- app/src/main/java/com/example/android_music_player/library/songs/Songs_Fr.java
+++ app/src/main/java/com/example/android_music_player/library/songs/Songs_Fr.java
@@ -1,6 +1,7 @@
 package com.example.android_music_player.library.songs;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,10 +12,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.example.android_music_player.AudioPlayer;
 import com.example.android_music_player.MainActivity;
 import com.example.android_music_player.R;
+import com.example.android_music_player.data.AudioModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,6 +54,33 @@ public class Songs_Fr extends Fragment {
 
                 //TODO implement play/pause
 
+                RelativeLayout songLayout = (RelativeLayout) v.getParent();
+                ImageView playIcon = songLayout.findViewById(R.id.play_icon);
+                ImageView pauseIcon = songLayout.findViewById(R.id.pause_icon);
+
+                AudioPlayer player = MainActivity.audioPlayer;
+
+                if( playIcon.getVisibility() == View.VISIBLE )
+                {
+                    playIcon.setVisibility(View.INVISIBLE);
+                    pauseIcon.setVisibility(View.VISIBLE);
+
+                    if(player.playingSongLayout != null && player.playingSongLayout != songLayout)
+                    {
+                        player.playingSongLayout.findViewById(R.id.play_icon).setVisibility(View.VISIBLE);
+                        player.playingSongLayout.findViewById(R.id.pause_icon).setVisibility(View.INVISIBLE);
+                    }
+
+                    player.play(MainActivity.mediaData.getSongsList().get(position));
+                    player.playingSongLayout = songLayout;
+                }
+                else
+                {
+                    playIcon.setVisibility(View.VISIBLE);
+                    pauseIcon.setVisibility(View.INVISIBLE);
+
+                    player.pause();
+                }
             }
 
             @Override
@@ -59,7 +91,7 @@ public class Songs_Fr extends Fragment {
             }
         });
 
-        recyclerView.setAdapter(songsRecyclerAdapter);
 
+        recyclerView.setAdapter(songsRecyclerAdapter);
     }
 }
